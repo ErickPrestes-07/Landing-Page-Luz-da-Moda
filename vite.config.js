3cc763c3941b0446b11e7b876fb600adab3e0fd5
ErickPrestes-07 <erickfelipe@certtus.com.br>
2026-05-05 11:34:55 -0300

    fix: adicionando plugins do react e tailwind no vite.config.js para corrigir build no vercel

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // config options
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
